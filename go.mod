0b98953b3a0e8e1ed8feb57aec94425d364e3395
Teruhiro Komaki <teruhirokomaki@gmail.com>
2020-06-27 23:49:41 +0900

    Fix to display FMDataMigration return values

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/frudens/gofmdatamigration
+
+go 1.14
+
+require github.com/urfave/cli/v2 v2.2.0 // indirect
--- gofmdatamigration.go
+++ gofmdatamigration.go
@@ -1,36 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
-	"os/exec"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"sort"
 	"strings"
-	"fmt"
-	"github.com/urfave/cli"
-	"path/filepath"
-)
 
-const fmDataMigration = "./FMDataMigration"
+	"github.com/urfave/cli/v2"
+)
 
 // path
 var clonePath string
 var prodPath string
 var migratedPath string
+var toolPath string
 
 // option
 var account string
 var pwd string
 var key string
+var force bool
 var ignore_valuelists bool
 var ignore_accounts bool
 var ignore_fonts bool
+var verbose bool
+var quiet bool
 
 // value
+var force_string string
 var ignore_valuelists_string string
 var ignore_accounts_string string
 var ignore_fonts_string string
+var verbose_string string
+var quiet_string string
 
 func getCloneDir(dir string) error {
 
@@ -56,7 +63,7 @@ func getCloneDir(dir string) error {
 
 		// filename ext
 		clonePath = filepath.Join(dir, file.Name()) /* resources/clone/dir/file Colne.fmp12 */
-		ext := filepath.Ext(clonePath) /* .fmp12 */
+		ext := filepath.Ext(clonePath)              /* .fmp12 */
 
 		// check .fmp12
 		if ext == ".fmp12" {
@@ -64,14 +71,14 @@ func getCloneDir(dir string) error {
 			clonePathTrim := r.Replace(clonePath) /* resources/clone/dir/file.fmp12 */
 
 			// prodpath migratedPath
-			prodPath = strings.Replace(clonePathTrim, "/clone", "/prod", -1) /* resources/prod/dir/file.fmp12 */
-			migratedPath = strings.Replace(clonePathTrim, "/clone", "/migrated", -1) /* resources/migrated/dir/file.fmp12 */
+			prodPath = strings.Replace(clonePathTrim, "clone", "prod", -1)         /* resources/prod/dir/file.fmp12 */
+			migratedPath = strings.Replace(clonePathTrim, "clone", "migrated", -1) /* resources/migrated/dir/file.fmp12 */
 
 			// mkdir for migrated
 			m := filepath.Dir(migratedPath) /* resources/migrated/dir */
 			_, err := os.Stat(m)
 			if err != nil {
-				err = os.MkdirAll(m,0777)
+				err = os.MkdirAll(m, 0777)
 				if err != nil {
 					return cli.NewExitError(err, 4)
 				}
@@ -90,9 +97,11 @@ func getCloneDir(dir string) error {
 
 func migration() {
 	// exec
-	b, err := exec.Command(fmDataMigration, "-src_path", prodPath, "-src_account", account, "-src_pwd", pwd, "-src_key", key,"-clone_path", clonePath, "-clone_account", account, "-clone_pwd", pwd, "-clone_key", key, "-target_path", migratedPath, ignore_valuelists_string, ignore_accounts_string, ignore_fonts_string).CombinedOutput()
+	cmd := exec.Command(toolPath, "-src_path", prodPath, "-src_account", account, "-src_pwd", pwd, "-src_key", key, "-clone_path", clonePath, "-clone_account", account, "-clone_pwd", pwd, "-clone_key", key, "-target_path", migratedPath, ignore_valuelists_string, ignore_accounts_string, ignore_fonts_string, force_string, verbose_string, quiet_string)
+
+	b, err := cmd.Output()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("FMDataMigration error:\n%s\n", string(b))
 	}
 
 	// log replace
@@ -113,51 +122,130 @@ func migration() {
 	fmt.Println(l)
 }
 
+func getToolPath() string {
+	d, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// check toolName
+	var toolName string
+	var toolDir string
+	if runtime.GOOS == "windows" {
+		toolName = "FMDataMigration.exe"
+	} else {
+		toolName = "FMDataMigration"
+	}
+	toolDir = "FMDataMigration"
+
+	var path string
+	if existDir(filepath.Join(d, toolDir)) {
+		// check tool dir
+		path = filepath.Join(d, toolDir, toolName)
+	} else {
+		// check current dir
+		path = filepath.Join(d, toolName)
+	}
+
+	if existFile(path) {
+		return path
+	} else {
+		return ""
+	}
+}
+
+func existFile(filename string) bool {
+	f, err := os.Stat(filename)
+	if os.IsNotExist(err) || f.IsDir() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func existDir(dir string) bool {
+	f, err := os.Stat(dir)
+	if os.IsNotExist(err) || !f.IsDir() {
+		return false
+	} else {
+		return true
+	}
+}
+
 func main() {
 
 	// app
 	app := cli.NewApp()
 	app.Name = "goFMDataMigration"
 	app.Usage = "goFMDataMigration is a command line tool for easy data migration using the FMDataMigration tool."
-	app.Version = "0.2.0"
-	app.Author = "frudens Inc. <https://frudens.com>"
+	app.Version = "0.3.0"
+	app.Authors = []*cli.Author{
+		&cli.Author{
+			Name:  "frudens Inc.",
+			Email: "komaki@frudens.com",
+		},
+	}
 
 	// global option
-	app.Flags = []cli.Flag {
-		cli.StringFlag{
-			Name: "account, a",
-			Usage: "source Full Access or FMMigration account",
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "account",
+			Aliases:     []string{"a"},
+			Usage:       "source Full Access or FMMigration account",
 			Destination: &account,
 		},
-		cli.StringFlag{
-			Name: "pwd, p",
-			Usage: "source Full Access or FMMigration password",
+		&cli.StringFlag{
+			Name:        "pwd",
+			Aliases:     []string{"p"},
+			Usage:       "source Full Access or FMMigration password",
 			Destination: &pwd,
 		},
-		cli.StringFlag{
-			Name: "key, k",
-			Usage: "source decryption key",
+		&cli.StringFlag{
+			Name:        "key",
+			Aliases:     []string{"k"},
+			Usage:       "source decryption key",
 			Destination: &key,
 		},
-		cli.BoolFlag{
-			Name: "ignore_valuelists, iv",
-			Usage: "use custom value lists from clone instead of source",
+		&cli.BoolFlag{
+			Name:        "force",
+			Usage:       "overwrite existing target file",
+			Destination: &force,
+		},
+		&cli.BoolFlag{
+			Name:        "ignore_valuelists",
+			Usage:       "use custom value lists from clone instead of source",
 			Destination: &ignore_valuelists,
 		},
-		cli.BoolFlag{
-			Name: "ignore_accounts, ia",
-			Usage: "use accounts and decryption key from clone instead of source",
+		&cli.BoolFlag{
+			Name:        "ignore_accounts",
+			Usage:       "use accounts and decryption key from clone instead of source",
 			Destination: &ignore_accounts,
 		},
-		cli.BoolFlag{
-			Name: "ignore_fonts, if",
-			Usage: "assume no font mapping required for field contents",
+		&cli.BoolFlag{
+			Name:        "ignore_fonts",
+			Usage:       "assume no font mapping required for field contents",
 			Destination: &ignore_fonts,
+		},
+		&cli.BoolFlag{
+			Name:        "verbose",
+			Usage:       "verbose mode",
+			Destination: &verbose,
+		},
+		&cli.BoolFlag{
+			Name:        "quiet",
+			Usage:       "quiet mode",
+			Destination: &quiet,
 		},
 	}
 
 	// action
 	app.Action = func(c *cli.Context) error {
+
+		// check toolPath
+		toolPath = getToolPath()
+		if toolPath == "" {
+			return cli.NewExitError("FMDataMigration not found in current directory.", 5)
+		}
 
 		// check account
 		if account == "" {
@@ -168,6 +256,9 @@ func main() {
 		cloneDir := filepath.Join("resources", "clone")
 
 		// set option
+		if force {
+			force_string = "-force"
+		}
 		if ignore_valuelists {
 			ignore_valuelists_string = "-ignore_valuelists"
 		}
@@ -176,6 +267,12 @@ func main() {
 		}
 		if ignore_fonts {
 			ignore_fonts_string = "-ignore_fonts"
+		}
+		if verbose {
+			verbose_string = "-v"
+		}
+		if quiet {
+			quiet_string = "-q"
 		}
 
 		// run
@@ -193,6 +290,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
